b0fcec1e731d6c5c8936e37355be59f6f0ca82f3
ashishsaxena7 <49167200+ashishsaxena7@users.noreply.github.com>
2019-05-19 17:20:23 -0500

    Add files via upload
    trying out login redirect with PaaS

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+const path = require('path')
+
+app.use(express.static(__dirname));
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}/`);
+});
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
